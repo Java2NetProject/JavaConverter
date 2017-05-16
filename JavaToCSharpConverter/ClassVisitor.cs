d5c5f7d8ede4d063872099fd7b083cc86af73dcb
Philemon Eichin <phileicht@outlook.de>
2017-05-16 13:42:17 +0200

    Add project files.

---
+++ JavaToCSharpConverter/ClassVisitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom;
+
+namespace JavaToCSharpConverter
+{
+    class ClassVisitor : Antlr4.Languages.Java.Java8BaseVisitor<CodeTypeDeclaration>
+    {
+        override VisitCl
+    }
+}
---
+++ JavaToCSharpConverter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavaToCSharpConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputStream = new Antlr4.Runtime.AntlrFileStream(args[0]);
+            var lexer = new Antlr4.Languages.Java.Java8Lexer(inputStream);
+            var ts = new Antlr4.Runtime.CommonTokenStream(lexer);
+            var parser = new Antlr4.Languages.Java.Java8Parser(ts);
+            var walker = new Antlr4.Runtime.Tree.ParseTreeWalker();
+ 
+
+
+        }
+    }
+}
